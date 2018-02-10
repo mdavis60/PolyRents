a2435321769bd1562419a3314c1c98aaa76e879c
Mitchel <mdavis60@calpoly.edu>
2018-02-10 14:22:49 -0800

    getters and setters for resourcetype

--- PolyRents/PolyRents/model/ResourceType.cs
+++ PolyRents/PolyRents/model/ResourceType.cs
@@ -12,5 +12,57 @@ namespace PolyRents.model
         private string resourceName;
         private float replacementCost;
         private float pastDueCost;
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public string ResourceName
+        {
+            get
+            {
+                return resourceName;
+            }
+
+            set
+            {
+                resourceName = value;
+            }
+        }
+
+        public float ReplacementCost
+        {
+            get
+            {
+                return replacementCost;
+            }
+
+            set
+            {
+                replacementCost = value;
+            }
+        }
+
+        public float PastDueCost
+        {
+            get
+            {
+                return pastDueCost;
+            }
+
+            set
+            {
+                pastDueCost = value;
+            }
+        }
     }
 }
